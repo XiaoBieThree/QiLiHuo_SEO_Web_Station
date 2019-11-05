012450c36dc18f18bf6ee210b89b0a063391e970
TongMutian <1125145878@qq.com>
2019-11-05 19:14:50 +0800

    Perfect home page's code

---
+++ src/components/index/data.js
@@ -0,0 +1,162 @@
+let notice = [{
+    id: 1,
+    dataTime: "2019-10-16",
+    href: "http://www.qlhseo.com:80/getDocDetail?nid=15",
+    contant: "新功能发布"
+  },
+  {
+    id: 2,
+    dataTime: "2019-10-16",
+    href: "http://www.qlhseo.com:80/getDocDetail?nid=16",
+    contant: "10.15企力火云市场最新资讯"
+  },
+  {
+    id: 3,
+    dataTime: "2019-10-16",
+    href: "http://www.qlhseo.com:80/getDocDetail?nid=17",
+    contant: "Paas云销系统来袭，颠覆传统营销"
+  },
+  {
+    id: 4,
+    dataTime: "2019-10-16",
+    href: "http://www.qlhseo.com:80/getDocDetail?nid=18",
+    contant: "VR商务营销手机新的裂变凸点"
+  },
+  {
+    id: 5,
+    dataTime: "2019-10-16",
+    href: "http://www.qlhseo.com:80/getDocDetail?nid=19",
+    contant: "业务合伙人须知，亲启"
+  },
+  {
+    id: 6,
+    dataTime: "2019-10-16",
+    href: "http://www.qlhseo.com:80/getDocDetail?nid=20",
+    contant: "欢迎加入众柴企力火"
+  }
+]
+let help_document = [{
+  id: 1,
+    href: 'http://www.qlhseo.com:80/getDocDetail?nid=22',
+    contant: '产品定价'
+  }, {
+    id: 2,
+    href: 'http://www.qlhseo.com:80/getDocDetail?nid=23',
+    contant: '备案帮助'
+  },
+  {
+    id: 3,
+    href: 'http://www.qlhseo.com:80/getDocDetail?nid=24',
+    contant: '入门指南'
+  },
+  {
+    id: 4,
+    href: 'http://www.qlhseo.com:80/getDocDetail?nid=25',
+    contant: '产品文档'
+  },
+  {
+    id: 5,
+    href: 'http://www.qlhseo.com:80/getDocDetail?nid=26',
+    contant: '开发者资源'
+  },
+  {
+    id: 6,
+    href: 'http://www.qlhseo.com:80/getDocDetail?nid=27',
+    contant: '财务相关'
+  }
+]
+let popular_product = [{
+  id: 1,
+    href: 'http://www.qlhseo.com:80/product_detail?id=4',
+    contant: '微商城'
+  },
+  {
+    id: 2,
+    href: 'http://www.qlhseo.com:80/product_detail?id=5',
+    contant: 'Paas云销系统'
+  },
+  {
+    id: 3,
+    href: 'http://www.qlhseo.com:80/product_detail?id=6',
+    contant: 'APP开发'
+  },
+  {
+    href: 'http://www.qlhseo.com:80/product_detail?id=7',
+    contant: '小源宝'
+  },
+  {
+    id: 4,
+    href: 'http://www.qlhseo.com:80/product_detail?id=8',
+    contant: '人机协作机器人'
+  },
+  {
+    id: 5,
+    href: 'http://www.qlhseo.com:80/product_detail?id=9',
+    contant: '霸屏推广'
+  }
+]
+let blogroll = [{
+  id: 1,
+  href: 'javascript:;',
+  contant: '网易云'
+}, {
+  id: 2,
+  href: 'javascript:;',
+  contant: '天猫'
+}, {
+  id: 3,
+  href: 'javascript:;',
+  contant: '百度'
+}, {
+  id: 4,
+  href: 'javascript:;',
+  contant: '京东'
+}, {
+  id: 5,
+  href: 'javascript:;',
+  contant: '今日头条'
+}, {
+  id: 6,
+  href: 'javascript:;',
+  contant: '新浪'
+}, {
+  id: 7,
+  href: 'javascript:;',
+  contant: '腾讯云'
+}]
+let hb_box_card = [
+  {
+    id: 1,
+    img_url: '../../assets/home/1571905521662.png',
+    title: '商务合作客服',
+    contant: '落座于上海，专业团队提供一对一商务合作及培训'
+  },
+  {
+    id: 2,
+    img_url: '../../assets/home/1571905545329.png',
+    title: '技术售后客服',
+    contant: '提供一对一技术咨询及功能优化，助你快速上云'
+  },
+  {
+    id: 3,
+    img_url: '../../assets/home/1571905563558.png',
+    title: '技术研发中心',
+    contant: '落座于苏州，成都，两个技术研发中心'
+  },
+  {
+    id: 4,
+    img_url: '../../assets/home/1571905579590.png',
+    title: '企业用户专区',
+    contant: '企业用户专属优惠与架构1对1服务'
+  }
+]
+let contact_us = ['客服电话4008-777-818转3', '售前电话4008-777-818转1', '售后QQ4008-777-818转2', '备案帮助4008-777-818转3', '商务合作18321567269', '违规投诉18321567269']
+let data = {
+  notice,
+  help_document,
+  popular_product,
+  contact_us,
+  blogroll,
+  hb_box_card
+}
+export default data
--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -40,42 +40,151 @@
       </el-row>
     </div>
     <div class="h-body">
-      <el-tabs v-model="activeName" @tab-click="tabsClick">
-        <!-- home-body 简写hb -->
-        <el-tab-pane label="首页" name="first" class="hb-tabs">首页</el-tab-pane>
-        <el-tab-pane label="最新活动" name="second" class="hb-tabs">
-          <router-view></router-view>
-        </el-tab-pane>
-        <el-tab-pane label="媒介" name="third" class="hb-tabs">媒介</el-tab-pane>
-        <el-tab-pane label="运维开发" name="fourth" class="hb-tabs">运维开发</el-tab-pane>
-        <el-tab-pane label="云市场" name="fifth" class="hb-tabs">云市场</el-tab-pane>
-        <el-tab-pane label="定价" name="sixth" class="hb-tabs">定价</el-tab-pane>
-        <el-tab-pane label="支持与服务" name="seventh" class="hb-tabs">支持与服务</el-tab-pane>
-        <el-tab-pane label="招商加盟" name="eighth" class="hb-tabs">招商加盟</el-tab-pane>
-        <el-tab-pane label="众柴学院" name="nineth" class="hb-tabs">众柴学院</el-tab-pane>
-        <el-tab-pane label="了解企力火" name="tenth" class="hb-tabs">了解企力火</el-tab-pane>
-      </el-tabs>
-      <!-- <router-view /> -->
-      <!-- 云市场分类
-              AI人工智能
-              电销机器人
-              人机协作系统
-              自动寻客系统
-           
-           PASS云销系统
-              智能线上客服
-              AI加装小程序
-              微信机器人
-           
-           搜索云计算
-              企力火整站优化
-              企力火新站优化
-              企力火万词霸屏
-           
-           软件开发服务
-              百度/微信小程序建设
-              移动应用(app)建设
-      商城开发建设-->
+      <div class="hb-top">
+        <el-tabs v-model="activeName" @tab-click="tabsClick">
+          <!-- home-body 简写hb -->
+          <el-tab-pane label="首页" name="first" class="hb-tabs">
+            <el-row>
+              <el-col :span="5">
+                <!-- <i class="el-icon-s-unfold"></i> -->
+                <h5 class="hb-h5">云市场分类</h5>
+                <el-menu
+                  default-active="2"
+                  class="el-menu-vertical-demo"
+                  @open="handleOpen"
+                  @close="handleClose"
+                  background-color="#000"
+                  text-color="#d3dce6"
+                  active-text-color="#fff"
+                >
+                  <el-submenu index="1">
+                    <template slot="title">
+                      <i class="el-icon-position"></i>
+                      <span class="hb-title-span">AI人工智能</span>
+                    </template>
+                    <el-menu-item index="1-1">电销机器人</el-menu-item>
+                    <el-menu-item index="1-2">人机协作系统</el-menu-item>
+                    <el-menu-item index="1-3">自动寻客系统</el-menu-item>
+                  </el-submenu>
+                  <el-submenu index="2">
+                    <template slot="title">
+                      <i class="el-icon-position"></i>
+                      <span class="hb-title-span">PASS云销系统</span>
+                    </template>
+                    <el-menu-item index="2-1">智能线上客服</el-menu-item>
+                    <el-menu-item index="2-2">AI加装小程序</el-menu-item>
+                    <el-menu-item index="2-3">微信机器人</el-menu-item>
+                  </el-submenu>
+                  <el-submenu index="3">
+                    <template slot="title">
+                      <i class="el-icon-position"></i>
+                      <span class="hb-title-span">搜索云计算</span>
+                    </template>
+                    <el-menu-item index="3-1">企力火整站优化</el-menu-item>
+                    <el-menu-item index="3-2">企力火新站优化</el-menu-item>
+                    <el-menu-item index="3-3">企力火万词霸屏</el-menu-item>
+                  </el-submenu>
+                  <el-submenu index="4">
+                    <template slot="title">
+                      <i class="el-icon-position"></i>
+                      <span class="hb-title-span">软件开发服务</span>
+                    </template>
+                    <el-menu-item index="4-1">百度/微信小程序建设</el-menu-item>
+                    <el-menu-item index="4-2">移动应用(app)建设</el-menu-item>
+                  </el-submenu>
+                </el-menu>
+              </el-col>
+              <el-col :span="19">
+                <div class="nav-image">
+                  <img src="../../assets/home/nav-image.png" alt />
+                </div>
+                <div class="hb-cards">
+                  <el-card
+                    class="hb-box-card"
+                    shadow="hover"
+                    v-for="(item, index) in hb_box_card"
+                    :key="index"
+                  >
+                    <div class="hb-box-card-f">{{item.title}}</div>
+                    <div class="hb-box-card-s">{{item.contant}}</div>
+                    <div class="hb-box-card-t">
+                      <img src="../../assets/home/1571905563558.png" />
+                    </div>
+                  </el-card>
+                </div>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="6">
+                <div class="hb-first-application_center-title">
+                  <p>企力火应用中心</p>
+                </div>
+                <div class="hb-f-application_center-left">
+                  <div>
+                    <span>买软件，选服务</span>
+                    <span>就上企力火云市场</span>
+                    <span>智能线上客服，自动拓客机器人等</span>
+                    <span>多数软件免费试用</span>
+                    <span>千套H5，微信小程序，模板网站（响应式）应有尽有，放心选择。</span>
+                  </div>
+                </div>
+              </el-col>
+              <el-col :span="18">
+                <div class="hb-f-application_center-right">
+                  <el-row v-for="index in 3" :key="index" class="hb-f-a-right-row">
+                    <el-col :span="8" v-for="(item, index) in 3" :key="index">
+                      <div class="hb-f-a-right-col"></div>
+                    </el-col>
+                  </el-row>
+                </div>
+              </el-col>
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="最新活动" name="second" class="hb-tabs">
+            <router-view></router-view>
+          </el-tab-pane>
+          <el-tab-pane label="媒介" name="third" class="hb-tabs"></el-tab-pane>
+          <el-tab-pane label="运维开发" name="fourth" class="hb-tabs">运维开发</el-tab-pane>
+          <el-tab-pane label="云市场" name="fifth" class="hb-tabs">云市场</el-tab-pane>
+          <el-tab-pane label="定价" name="sixth" class="hb-tabs">定价</el-tab-pane>
+          <el-tab-pane label="支持与服务" name="seventh" class="hb-tabs">支持与服务</el-tab-pane>
+          <el-tab-pane label="招商加盟" name="eighth" class="hb-tabs">招商加盟</el-tab-pane>
+          <el-tab-pane label="众柴学院" name="nineth" class="hb-tabs">众柴学院</el-tab-pane>
+          <el-tab-pane label="了解企力火" name="tenth" class="hb-tabs">了解企力火</el-tab-pane>
+        </el-tabs>
+      </div>
+      <div class="hb-bottom">
+        <div class="case-content-bg case-content-info">
+          <div class="case-box">
+            <h3 class="case-box-title">
+              <em>企力火应用中心</em>
+              <span>各行各业的合作伙伴使用大数据分析产品</span>
+            </h3>
+            <div class="case-wrapper clearfix" v-for="index in 2" :key="index">
+              <div class="case-item" v-for="(item, index) in 4" :key="index">
+                <div class="case-item-bg" :class="'case-item-bg' + (index + 1)"></div>
+                <div class="case-item-title">Heading information {{index + 1}}</div>
+                <div
+                  class="case-item-text"
+                >应用BML做商户点击率的预估，提升了模型训练效率，基于海量数据做到了模型的每天更新。上线后转化率比之前有明显提升。</div>
+                <div class="case-item-doc">
+                  <p>使用产品</p>
+                  <p>
+                    <!-- <a href="#">查看详情</a> -->
+                    <!-- <a href="#">立即咨询</a> -->
+                    <el-badge value="hot" class="item">
+                      <el-button size="small">查看详情</el-button>
+                    </el-badge>
+                    <el-badge value="有惊喜" class="item">
+                      <el-button size="small">立即咨询</el-button>
+                    </el-badge>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="h-footer">
       <div class="hf-first">
@@ -83,111 +192,31 @@
           <div class="hf-first-1">
             <ul>
               <li style="font-size: 16px;color: white;margin-bottom:20px;">公告</li>
-              <li>
-                <a
-                  href="http://www.qlhseo.com:80/getDocDetail?nid=15"
-                  target="_blank"
-                >[2019-10-16]&nbsp;新功能发布</a>
-              </li>
-              <li>
-                <a
-                  href="http://www.qlhseo.com:80/getDocDetail?nid=16"
-                  target="_blank"
-                >[2019-10-16]&nbsp;10.15企力火云市场最新资讯</a>
-              </li>
-              <li>
-                <a
-                  href="http://www.qlhseo.com:80/getDocDetail?nid=17"
-                  target="_blank"
-                >[2019-10-16]&nbsp;Paas云销系统来袭，颠覆传统营销</a>
-              </li>
-              <li>
-                <a
-                  href="http://www.qlhseo.com:80/getDocDetail?nid=18"
-                  target="_blank"
-                >[2019-10-16]&nbsp;VR商务营销手机新的裂变凸点</a>
-              </li>
-              <li>
-                <a
-                  href="http://www.qlhseo.com:80/getDocDetail?nid=19"
-                  target="_blank"
-                >[2019-10-16]&nbsp;业务合伙人须知，亲启</a>
-              </li>
-              <li>
-                <a
-                  href="http://www.qlhseo.com:80/getDocDetail?nid=20"
-                  target="_blank"
-                >[2019-10-16]&nbsp;欢迎加入众柴企力火</a>
+              <li v-for="(item, index) in notice" :key="index">
+                <a :href="item.href" target="_blank">[{{item.dataTime}}]&nbsp;{{item.contant}}</a>
               </li>
             </ul>
           </div>
           <div class="hf-first-2">
             <ul>
               <li style="font-size: 16px;color: white;margin-bottom:20px;">帮助文档</li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/getDocDetail?nid=22" target="_blank">产品定价</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/getDocDetail?nid=23" target="_blank">备案帮助</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/getDocDetail?nid=24" target="_blank">入门指南</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/getDocDetail?nid=25" target="_blank">产品文档</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/getDocDetail?nid=26" target="_blank">开发者资源</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/getDocDetail?nid=27" target="_blank">财务相关</a>
+              <li v-for="(item, index) in help_document" :key="index">
+                <a :href="item.href" target="_blank">{{item.contant}}</a>
               </li>
             </ul>
           </div>
           <div class="hf-first-3">
             <ul>
               <li style="font-size: 16px;color: white;margin-bottom:20px;">热门产品</li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/product_detail?id=4" target="_blank">微商城</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/product_detail?id=5" target="_blank">人机协作机器人</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/product_detail?id=6" target="_blank">APP开发</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/product_detail?id=7" target="_blank">小源宝</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/product_detail?id=8" target="_blank">人机协作机器人</a>
-              </li>
-
-              <li>
-                <a href="http://www.qlhseo.com:80/product_detail?id=9" target="_blank">霸屏推广</a>
+              <li v-for="(item, index) in popular_product" :key="index">
+                <a :href="item.href" target="_blank">{{item.contant}}</a>
               </li>
             </ul>
           </div>
           <div class="hf-first-4">
             <ul>
               <li style="font-size: 16px;color: white;margin-bottom:20px;">联系我们</li>
-              <li>客服电话4008-777-818转3</li>
-                                      <li>售前电话4008-777-818转1</li>
-              <li>售后QQ4008-777-818转2</li>
-              <li>备案帮助4008-777-818转3</li>
-              <li>商务合作18321567269</li>
-              <li>违规投诉18321567269</li>
+              <li v-for="(item, index) in contact_us" :key="index">{{item}}</li>
             </ul>
           </div>
           <div class="hf-first-5">
@@ -195,8 +224,8 @@
               <li style="font-size: 18px;color: white;margin-bottom:20px;">4008-777-818转3</li>
               <li>24小时售前咨询</li>
               <li style="height: 45px;margin-top:50px;">
-                <img src="http://www.qlhseo.com:80/image/part9_wx_1.png" style="height: 45px;" />
-                <img src="http://www.qlhseo.com:80/image/part9_app-store.png" style="height: 40px;" />
+                <img src="../../assets/home/wx_1.png" style="height: 45px;" />
+                <img src="../../assets/home/app-store.png" style="height: 40px;" />
               </li>
             </ul>
           </div>
@@ -204,41 +233,50 @@
       </div>
       <div class="hf-second">
         <ul>
-          <li><a href="javascript:;">友情链接：</a></li>
-          <li><a href="javascript:;">网易云</a></li>
-          <li><a href="javascript:;">天猫</a></li>
-          <li><a href="javascript:;">百度</a></li>
-          <li><a href="javascript:;">京东</a></li>
-          <li><a href="javascript:;">今日头条</a></li>
-          <li><a href="javascript:;">新浪</a></li>
-          <li><a href="javascript:;">腾讯云</a></li>
+          <li>
+            <a href="javascript:;">友情链接：</a>
+          </li>
+          <li v-for="(item, index) in blogroll" :key="index">
+            <a :href="item.href">{{item.contant}}</a>
+          </li>
         </ul>
       </div>
-      <div class="hf-end">
-        © {{this.year}} 上海众柴软件科技有限公司 使用百度前必读 京ICP证030173号
-      </div>
+      <div class="hf-end">© {{this.year}} 上海众柴软件科技有限公司 使用百度前必读 京ICP证030173号</div>
     </div>
   </div>
 </template>
 
 <script>
 import moment from "moment";
+import data from "./data.js";
 export default {
   data() {
     return {
       input: "",
-      activeName: "second",
-      year: moment().format("YYYY")
+      activeName: "first",
+      year: moment().format("YYYY"),
+      notice: data.notice,
+      help_document: data.help_document,
+      popular_product: data.popular_product,
+      contact_us: data.contact_us,
+      blogroll: data.blogroll,
+      hb_box_card: data.hb_box_card
     };
   },
   methods: {
     tabsClick(tab, event) {
       console.log(tab, event);
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
 };
 </script>
 
 <style lang='less'>
-  @import "./index.less"; 
+@import "./index.less";
 </style>
